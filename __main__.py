83ffa021e7bbff1538cb95795f548d9a6950f99a
Marciax <35850572+Marciax@users.noreply.github.com>
2018-01-26 21:12:42 +0100

    Add files via upload

---
+++ __main__.py
@@ -0,0 +1,9 @@
+waga = float(input('Podaj wage'))
+wzrost = float(input('Podaj wzrost'))
+
+wzrost_do_kwadratu = wzrost**2
+bmi = waga / wzrost_do_kwadratu*10000
+print("Twoje bmi wynosi" + str(bmi))
+
+
+
